cbe0b93a31dcc3f9b731475d37d2b764d673708f
xiaoqing zheng <e1124735@u.nus.edu>
2024-10-22 14:29:33 +0800

    change the function of this script to separate the unmapped list

--- src/countunmapped.sh
+++ src/countunmapped.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-READS_FILE="/mnt/gtklab01/xiaoqing/sample_name.txt"
-RESULT_DIR="/mnt/gtklab01/xiaoqing/salmon/full_output"
+## the actual name of the script directory
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source "${SCRIPT_DIR}/atlas-config.sh"
+
 NAME_SUF="aux_info/unmapped_names.txt"
 while read SAMPLE_NAME; do
-    echo ${SAMPLE_NAME}
-    cut -d" " -f 2 $RESULT_DIR/$SAMPLE_NAME/$NAME_SUF | sort | uniq -c
+    echo "start working on ${SAMPLE_NAME}."
+    while IFS= read -r line; do
+        tag=$(echo "$line" | cut -d" " -f2)
+        seqid=$(echo "$line" | cut -d" " -f1)
+        echo "$seqid" >> "$SALMON_OUTPUT_DIR/$SAMPLE_NAME/"aux_info/unmapped"_${tag}.lst"
+    done < $SALMON_OUTPUT_DIR/$SAMPLE_NAME/$NAME_SUF
 done < $READS_FILE

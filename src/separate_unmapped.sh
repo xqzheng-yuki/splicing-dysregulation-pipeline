e61300f752c005f478f8d4fcbbbe68fc1fef0037
xiaoqing zheng <e1124735@u.nus.edu>
2024-10-22 16:02:21 +0800

    change for common use

--- src/separate_unmapped.sh
+++ src/separate_unmapped.sh
@@ -1,26 +1,28 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+# File: separate_unmapped.sh
+# Purpose: This script group sequences that tagged d, m1, m2, u into separate file.
+# Dependencies: bash
+# Input: unmapped_names.txt as the output of Salmon SA mode
+# Output: Text files containing each type of unmapped sequence name, with each file saved as .lst
+# Usage: bash separate_unmapped.sh
+# Version History:
+# v1.0 - Initial test release
+# v2.0 - Initial general release
+# Note: Ensure that the directory is the same or redirect by your need
 
 ## the actual name of the script directory
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "${SCRIPT_DIR}/atlas-config.sh"
 PROJECT_DIR=$(dirname ${SCRIPT_DIR})
 
-echo $PROJECT_DIR
-
-SALMON_OUTPUT_DIR=${PROJECT_DIR}/test_results
-mkdir -p $SALMON_OUTPUT_DIR
-
-READS_FILE=${PROJECT_DIR}/test_data/reads_file.txt
-
 while read SAMPLE_NAME; do
     echo "start working on ${SAMPLE_NAME}"
-    OUTPUT_DIR="$SALMON_OUTPUT_DIR/$SAMPLE_NAME/aux_info"
-    mkdir -p $OUTPUT_DIR
-    INPUT_FILE="${PROJECT_DIR}/test_data/${SAMPLE_NAME}_part_unmapped_name.txt"
-    ls $INPUT_FILE
+    cd $RESULT_DIR/${SAMPLE_NAME}/"aux_info"
+    INPUT_FILE="$RESULT_DIR/${SAMPLE_NAME}/aux_info/unmapped_names.txt"
     for UNMAPPED_TYPE in d m1 m2 u ; do
-	echo "working on $UNMAPPED_TYPE"
-	OUTPUT_FILE="$OUTPUT_DIR/unmapped_${UNMAPPED_TYPE}.lst"
-	grep "${UNMAPPED_TYPE}$" $INPUT_FILE | cut -f1 -d" " > $OUTPUT_FILE
+	    echo "working on $UNMAPPED_TYPE"
+	    OUTPUT_FILE="./unmapped_${UNMAPPED_TYPE}.lst"
+	    grep "${UNMAPPED_TYPE}$" $INPUT_FILE | cut -f1 -d" " > $OUTPUT_FILE
     done
 done < $READS_FILE

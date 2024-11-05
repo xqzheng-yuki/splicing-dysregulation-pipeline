bbf4304503d9aeda74c5c7d1c5a34a3468262807
xiaoqing zheng <e1124735@u.nus.edu>
2024-11-05 17:46:50 +0800

    update

---
+++ src/test.r
@@ -0,0 +1,26 @@
+import_bigwig <- function(file,bw_selection) {
+  bw_data <- BRGenomics::makeGRangesBRG(import.bw(file,selection=rtracklayer::BigWigSelection(bw_selection)))
+  keepStandardChromosomes(bw_data,pruning.mode = "coarse")
+}
+
+import_bigwig_ori <- function(file) {
+  bw_data <- import.bw(file)
+  keepStandardChromosomes(bw_data,pruning.mode = "coarse")
+}
+
+import_bigwig("/mnt/gtklab01/xiaoqing/star/results/group/unmapped_CTX_154_m1.bw",
+              bw_selection=gr)
+import_bigwig_ori("/mnt/gtklab01/xiaoqing/star/results/group/unmapped_CTX_154_m1.bw")
+
+
+foo <- control_bw_data[[1]]
+
+sapply(map(foo,function(x) x$score),sum)
+
+show_track<-list()
+show_track <- append(show_track,list(idt))
+show_track <- append(show_track,list(grTrack))
+show_track <- append(show_track,control_datatrack)
+show_track <- append(show_track,ko_datatrack)
+show_track <- append(show_track,list(gaxis))
+plotTracks(show_track, cex.sampleNames = 0.6)

8ded163da93c6aab827de7675047cc42f83e7dbb
xiaoqing zheng <e1124735@u.nus.edu>
2024-12-09 16:54:48 +0800

    feat: new snakemake for the main process

--- src/salmon-index.sh
+++ src/salmon-index.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-## the actual name of the script directory
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source "${SCRIPT_DIR}/atlas-config.sh"
+# ## the actual name of the script directory
+# SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# source "${SCRIPT_DIR}/atlas-config.sh"
 
-outfolder="${XQ_DIR}/salmon/index/decoy2"
-cd $outfolder
+# outfolder="${XQ_DIR}/salmon/index/decoy2"
+# cd $outfolder
+
+gentrome=$1
+decoy=$2
+output=$3
 
 # activate conda environment
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate salmon
-salmon index -t gentrome.fa --decoys decoysNintronic.txt -k 31 -p 8 -i salmon_index
+salmon index -t $gentrome --decoys $decoy -k 31 -p 8 -i $output
+conda deactivate
--- src/salmon.sh
+++ src/salmon.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
-set -o pipefail
-## the actual name of the script directory
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# set -o pipefail
+# ## the actual name of the script directory
+# SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# source "${SCRIPT_DIR}/atlas-config.sh"
 
-source "${SCRIPT_DIR}/atlas-config.sh"
+# mkdir -p $SALMON_OUTPUT_DIR/$DATE/"mapping"
 
-mkdir -p $SALMON_OUTPUT_DIR/$DATE
+INDEX_DIR=$1
+SAMPLE_NAME=$2
+READS_1=$3
+READS_2=$4
+OUTPUT_DIR=$5
 
 # activate conda environment
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate salmon
 
-# read input read files from separate file
-echo $READS_FILE
-INDEX_DIR="/mnt/gtklab01/xiaoqing/salmon/index/decoy2/salmon_index"
+# # read input read files from separate file
+# echo $READS_FILE
+# INDEX_DIR="/mnt/gtklab01/xiaoqing/salmon/index/decoy5/salmon_index"
+
 # main body
 # run salmon for each sample selective alignment (decoy mode)
-while read SAMPLE_NAME; do
-    salmon quant -l A -i $INDEX_DIR \
-    -1 $RNA_READS_DIR/${SAMPLE_NAME}_1.fq.gz \
-    -2 $RNA_READS_DIR/${SAMPLE_NAME}_2.fq.gz \
+# while read SAMPLE_NAME; do
+    salmon quant -l A -i "${INDEX_DIR}" \
+    -1 "${READS_1}" \
+    -2 "${READS_2}" \
     --useVBOpt --writeUnmappedNames \
-    -o $SALMON_OUTPUT_DIR/$DATE/$SAMPLE_NAME --threads 8
-    done < $READS_FILE
+    --writeMappings="${OUTPUT_DIR}/${SAMPLE_NAME}/aux_info/mapping.bam" \
+    -o "${OUTPUT_DIR}/${SAMPLE_NAME}" --threads 8
+    # done < $READS_FILE
 
+conda deactivate

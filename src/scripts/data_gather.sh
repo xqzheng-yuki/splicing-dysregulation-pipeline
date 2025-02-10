76352368eec5d0177fb2e3225d405cbbd274e1ab
xiaoqing zheng <e1124735@u.nus.edu>
2025-02-10 23:32:42 +0800

    add m1,m2 lst cleaning, ensuring m1,m2 lst available for later use.

--- src/scripts/data_gather.sh
+++ src/scripts/data_gather.sh
@@ -1,42 +1,48 @@
 #!/bin/bash
 
 bash lst_gather.sh
+samples=("CTX_104" "CTX_108" "CTX_120" "CTX_125" "CTX_128" "CTX_147" "CTX_148" "CTX_154")
+tags=("d" "m1" "m2")
+mkdir -p "/mnt/gtklab01/xiaoqing/2025-01-14-list_filter/analysis/count"
 
 # comm -23 <(sort CTX_104-unmapped_d.lst) <(sort CTX_104_mm-unmapped_d.lst) > analysis/CTX_104_d.lst
-for sample in CTX_*; do
-    prefix="${sample%-unmapped_d.lst}"
-    if [[ -f "${prefix}-unmapped_d.lst" && -f "${prefix}_mm-unmapped_d.lst" ]]; then
-        comm -23 <(sort "${prefix}-unmapped_d.lst") <(sort "${prefix}_mm-unmapped_d.lst") > "analysis/${prefix}_d.lst"
-        echo "Processed ${prefix}"
-    else
-        echo "Skipping ${prefix} due to missing files"
-    fi
+for sample in "${samples[@]}"; do
+    echo "Processed ${sample}"
+    for tag in "${tags[@]}"; do
+        if [[ -f "${sample}-unmapped_${tag}.lst" && -f "${sample}_mm-unmapped_${tag}.lst" ]]; then
+            comm -23 <(sort "${sample}-unmapped_${tag}.lst") <(sort "${sample}_mm-unmapped_${tag}.lst") > "analysis/${sample}_${tag}.lst"
+        else
+            echo "Skipping ${sample} due to missing files"
+        fi
+    done
 done
 
 # samtools view -N /mnt/gtklab01/xiaoqing/2025-01-14-list_filter/analysis/CTX_104_d.lst d.sorted.bam
-samples=("CTX_104" "CTX_108" "CTX_120" "CTX_125" "CTX_128" "CTX_147" "CTX_148" "CTX_154")
 for sample in "${samples[@]}"; do
-    bam="/mnt/gtklab01/xiaoqing/2025-01-14/star/result/${sample%}/d.sorted.bam"
-    lst="/mnt/gtklab01/xiaoqing/2025-01-14-list_filter/analysis/${sample%}_d.lst"
-    filter_re="/mnt/gtklab01/xiaoqing/2025-01-14/filter/${sample%}_d.bam"
-    filter_bw="/mnt/gtklab01/xiaoqing/2025-01-14/filter/bw/${sample%}_d.bw"
-    samtools view -N ${lst} -b ${bam} > ${filter_re}
-    samtools index ${filter_re}
-    bamCoverage -b ${filter_re} -o ${filter_bw}
+    for tag in "${tags[@]}"; do
+        bam="/mnt/gtklab01/xiaoqing/2025-01-14/star/result/${sample%}/${tag}.sorted.bam"
+        lst="/mnt/gtklab01/xiaoqing/2025-01-14-list_filter/analysis/${sample%}_${tag}.lst"
+        filter_re="/mnt/gtklab01/xiaoqing/2025-01-14/filter/${sample%}_${tag}.bam"
+        filter_bw="/mnt/gtklab01/xiaoqing/2025-01-14/filter/bw/${sample%}_${tag}.bw"
+        samtools view -N ${lst} -b ${bam} > ${filter_re}
+        samtools index ${filter_re}
+        bamCoverage -b ${filter_re} -o ${filter_bw}
+    done
 done
 
 # coverage
 # samtools view -N /mnt/gtklab01/xiaoqing/2025-01-14-list_filter/analysis/CTX_104_d.lst -F 256  mapping.bam | cut -f3 | uniq -c | grep -v "," | awk '{print $2 "\t" $1}' > count_d.tsv
 # samtools view -N /mnt/gtklab01/xiaoqing/2025-01-14-list_filter/analysis/CTX_104_d.lst -F 256  mapping.bam | cut -f3 | uniq -c | grep "," | awk '{print $2 "\t" $1}' > count_d_o.tsv
-samples=("CTX_104" "CTX_108" "CTX_120" "CTX_125" "CTX_128" "CTX_147" "CTX_148" "CTX_154")
-tags=("d" "m1" "m2")
-mkdir -p "/mnt/gtklab01/xiaoqing/2025-01-14-list_filter/analysis/count"
+
 for sample in "${samples[@]}"; do
     # pre-filter for primary alignment
     ## beware: the bam file are different from the one use for bw
         bam="/mnt/gtklab01/xiaoqing/2025-01-14/salmon/${sample}/aux_info/mapping.bam"
         filter_bam="/mnt/gtklab01/xiaoqing/2025-01-14/salmon/${sample}/aux_info/primary_mapping.bam"
-        samtools sort ${bam} | samtools view -F 256 -b -o ${filter_bam} --write-index
+        if [ ! -e ${filter_bam} ]; then
+            echo "${filter_bam} does not exist."
+            samtools sort ${bam} | samtools view -F 256 -b -o ${filter_bam} --write-index
+        fi
     # get count for different tag
     for tag in "${tags[@]}"; do
         lst="/mnt/gtklab01/xiaoqing/2025-01-14-list_filter/analysis/${sample}_${tag}.lst"

3df5371d9cd9c7d72a2f1aeb97f86150186436f9
xiaoqing zheng <e1124735@u.nus.edu>
2025-02-10 18:40:26 +0800

    add part to count depth

--- src/scripts/data_gather.sh
+++ src/scripts/data_gather.sh
@@ -24,3 +24,23 @@ for sample in "${samples[@]}"; do
     samtools index ${filter_re}
     bamCoverage -b ${filter_re} -o ${filter_bw}
 done
+
+# coverage
+# samtools view -N /mnt/gtklab01/xiaoqing/2025-01-14-list_filter/analysis/CTX_104_d.lst -F 256  mapping.bam | cut -f3 | uniq -c | grep -v "," | awk '{print $2 "\t" $1}' > count_d.tsv
+# samtools view -N /mnt/gtklab01/xiaoqing/2025-01-14-list_filter/analysis/CTX_104_d.lst -F 256  mapping.bam | cut -f3 | uniq -c | grep "," | awk '{print $2 "\t" $1}' > count_d_o.tsv
+samples=("CTX_104" "CTX_108" "CTX_120" "CTX_125" "CTX_128" "CTX_147" "CTX_148" "CTX_154")
+tags=("d" "m1" "m2")
+mkdir -p "/mnt/gtklab01/xiaoqing/2025-01-14-list_filter/analysis/count"
+for sample in "${samples[@]}"; do
+    # pre-filter for primary alignment
+    ## beware: the bam file are different from the one use for bw
+        bam="/mnt/gtklab01/xiaoqing/2025-01-14/salmon/${sample}/aux_info/mapping.bam"
+        filter_bam="/mnt/gtklab01/xiaoqing/2025-01-14/salmon/${sample}/aux_info/primary_mapping.bam"
+        samtools sort ${bam} | samtools view -F 256 -b -o ${filter_bam} --write-index
+    # get count for different tag
+    for tag in "${tags[@]}"; do
+        lst="/mnt/gtklab01/xiaoqing/2025-01-14-list_filter/analysis/${sample}_${tag}.lst"
+        tsv_name="/mnt/gtklab01/xiaoqing/2025-01-14-list_filter/analysis/count/count_${sample}_${tag}.tsv"
+        samtools view -N ${lst} ${filter_bam} | cut -f3 | uniq -c | grep -v "," | awk '{print $2 "\t" $1}' > ${tsv_name}
+    done
+done
